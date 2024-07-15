59cfd4c059d03304b1030e6af788f496d4d0498b
vlikitha37 <vlikitha37@gmail.com>
2024-07-15 14:56:45 +0100

    init commit

---
+++ back end/app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+app.get("/", (req,res)=>{
+    res.send("Hi, I am live ");
+
+});
+
+// const start = async () => {
+ 
+//     try{
+//         app.listen(PORT, () => {
+//           console.log(`${PORT} Yes I am connected`);
+//         });
+//     }catch(error){
+//         console.log(error);
+//     }
+// };
+
+// start();
+
+app.listen(PORT,()=>{
+    console.log("Working");
+})
